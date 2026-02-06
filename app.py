d080edd6e988a4cf0b95187edf93767440f1a87a
cisarovalea <153274518+cisarovalea@users.noreply.github.com>
2026-02-06 22:17:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if uploaded_file:
     
     stopwords_input = st.text_area(
         "Vlastné stop slová (oddelené čiarkou)",
-        "bakalársky,cieľ,práca,analýza,časť,praktický,teória,teoretický"
+        "bakalársky,cieľ,práca,analýza,časť,praktický,teória,teoretický,jednotlivý,prirodzený"
     )
 
     custom_stopwords = [w.strip().lower() for w in stopwords_input.split(",") if w.strip() != ""]
