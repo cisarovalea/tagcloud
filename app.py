f58c1869e77ece53b27c88b5213bb54fa97e40d5
cisarovalea <153274518+cisarovalea@users.noreply.github.com>
2026-02-06 22:14:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ slovak_stopwords = [
     "že", "zo", "za", "na", "v", "do", "po", "pre", "od", "k", "so", "bez", "o",
     "alebo", "tak", "ten", "tá", "to", "s", "sa", "sú", "sa", "už", "do", "pri",
     "sa", "ako", "pretože", "keď", "ktorý", "ktorá", "ktoré", "ale", "teda", "táto",
-    "podľa", "napriek", "byť", "zároveň", "vrátane", "mnohý", "určený", "toto"
+    "podľa", "napriek", "byť", "zároveň", "vrátane", "mnohý", "určený", "toto", "ona", "on"
 ]
 
 def lemmatize_words_udpipe(text):
